bbff1d5c08f6c3057dd797ce71ddf1f526877bd9
soonyemma <69128107+soonyemma@users.noreply.github.com>
2022-06-19 12:23:38 +0100

    Update wsgi.py

--- I4G013601FQP/I4G013601FQP/wsgi.py
+++ I4G013601FQP/I4G013601FQP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for I4G031360MCY project.
+WSGI config for I4G013601FQP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G031360MCY.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'I4G013601FQP.settings')
 
 application = get_wsgi_application()
